a750931b15bc9d0e61ccb4305652eb70dd3df7da
Swatar <35310160+Swatar@users.noreply.github.com>
2018-01-10 23:01:50 +0300

    init commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+$(window).scroll(function(){
+if ($(this).scrollTop() > 400) {
+$('.scrollup').fadeIn();
+} else {
+$('.scrollup').fadeOut();
+}
+});
+
+$('.scrollup').click(function(){
+$("html, body").animate({ scrollTop: -100 }, 400);
+return false;
+});
+
+});
+
+
+
+function openbox(id){
+    display = document.getElementById(id).style.display;
+
+    if(display=='none'){
+       document.getElementById(id).style.display='block';
+    }else{
+       document.getElementById(id).style.display='none';
+    }
+}
